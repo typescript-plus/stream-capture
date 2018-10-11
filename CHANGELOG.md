9f4888ad55cd7100526642edce4418616d97a5ed
mosop <mosop@users.noreply.github.com>
2018-10-11 20:24:59 +0900

    update libraries

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,9 +13,15 @@ Based on [Keep a Changelog].
 
 ## Versions
 
-### [Edge (HEAD)][edge]
+### Edge (HEAD)
 
-### [0.2.0]
+### 0.3.0
+
+#### Changed
+
+* updated libraries.
+
+### 0.2.0
 
 #### Removed
 
@@ -23,5 +29,3 @@ Based on [Keep a Changelog].
 
 [Keep a Changelog]: http://keepachangelog.com/en/1.0.0/
 [Semantic Versioning Caret]: https://github.com/myowncake/semver-caret
-[edge]: https://github.com/typescript-plus/stream-capture/compare/v0.2.0...HEAD
-[0.2.0]: https://github.com/typescript-plus/stream-capture/compare/v0.1.0...v0.2.0
--- spec/readme/usage/async.spec.ts
+++ spec/readme/usage/async.spec.ts
@@ -1,24 +1,26 @@
 import * as fs from 'fs';
 import * as path from 'path';
+// tslint:disable-next-line:no-implicit-dependencies
 import * as tmp from 'tmp';
+
 import { capture } from '../../../src';
 
 tmp.setGracefulCleanup();
 const tmpdir = tmp.dirSync().name;
-const FILE = path.join(tmpdir, 'file');
+const FILE = path.join(tmpdir, "file");
 
 async function write(stream: fs.WriteStream) {
-  stream.write(':D');
+  stream.write(":D");
 }
 
-it('README - Usage - Async (Promise)', async () => {
+it("README - Usage - Async (Promise)", async () => {
   const stream = fs.createWriteStream(FILE);
-  const captured = await capture(stream, async (buffer) => {
+  const captured = await capture(stream, async buffer => {
     await write(stream);
-    return buffer.join('');
+    return buffer.join("");
   });
-  expect(captured).toEqual(':D'); // true
-  stream.end(':P', () => {
-    expect(fs.readFileSync(FILE).toString()).toEqual(':P');
+  expect(captured).toEqual(":D"); // true
+  stream.end(":P", () => {
+    expect(fs.readFileSync(FILE).toString()).toEqual(":P");
   });
 });
