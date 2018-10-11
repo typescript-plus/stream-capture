0084d9c4ba272f709f443b7348e9050bdf90d27a
mosop <mosop@users.noreply.github.com>
2018-10-11 20:45:25 +0900

    0.3.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="node" />
-import { Writable } from 'stream';
 export declare type SyncCallback<T> = (buffer: string[]) => T;
 export declare type AsyncCallback<T> = (buffer: string[]) => Promise<T>;
 export declare type Callback<T> = SyncCallback<T> | AsyncCallback<T>;
 export interface Chunk {
     toString(...args: any[]): string;
 }
-export declare function capture<T>(writable: Writable, callback: (buffer: string[]) => Promise<T>): Promise<T>;
-export declare function capture<T>(writable: Writable, callback: (buffer: string[]) => T): T;
+export declare function capture<T>(writable: NodeJS.WritableStream, callback: (buffer: string[]) => Promise<T>): Promise<T>;
+export declare function capture<T>(writable: NodeJS.WritableStream, callback: (buffer: string[]) => T): T;
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// tslint:disable-next-line:promise-function-async
 function capture(writable, callback) {
     var original = writable.write;
     try {
@@ -13,12 +12,14 @@ function capture(writable, callback) {
             return write.apply(void 0, [buffer_1, chunk].concat(args));
         };
         var result_1 = callback(buffer_1);
-        if ((typeof result_1 === 'object' || typeof result_1 === 'function') && typeof result_1.then === 'function') {
+        if (typeof result_1.then === 'function') {
             return new Promise(function (resolve, reject) {
-                result_1.then(function (value) {
+                result_1
+                    .then(function (value) {
                     writable.write = original;
                     resolve(value);
-                }).catch(function (reason) {
+                })
+                    .catch(function (reason) {
                     writable.write = original;
                     reject(reason);
                 });
@@ -38,11 +39,9 @@ function write(buffer, chunk) {
     for (var _i = 2; _i < arguments.length; _i++) {
         args[_i - 2] = arguments[_i];
     }
-    // tslint:disable-next-line:no-unused-variable
-    var encoding;
     var callback;
     if (typeof args[0] === 'string') {
-        encoding = args.shift();
+        args.shift();
     }
     if (typeof args[0] === 'function') {
         callback = args.shift();
@@ -53,30 +52,4 @@ function write(buffer, chunk) {
     }
     return true;
 }
-// export async function captureAsync(writable: Writable, callback: () => Promise<void>): Promise<string> {
-//   const original = writable.write;
-//   try {
-//     const buffer: string[] = [];
-//     writable.write = (chunk: {toString(...args: any[]): string}, ...args: any[]) => {
-//       // tslint:disable-next-line:no-unused-variable
-//       let encoding: string | undefined;
-//       let cb: (() => void) | undefined;
-//       if (typeof args[0] === 'string') {
-//         encoding = args.shift() as string;
-//       }
-//       if (typeof args[0] === 'function') {
-//         cb = args.shift() as () => void;
-//       }
-//       buffer.push(chunk.toString());
-//       if (cb !== undefined) {
-//         cb();
-//       }
-//       return true;
-//     };
-//     await callback();
-//     return buffer.join('');
-//   } finally {
-//     writable.write = original;
-//   }
-// }
 //# sourceMappingURL=index.js.map
