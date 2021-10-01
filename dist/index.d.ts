50e1f1e84a30d480ab3169263244204a584a9acf
mosop <mosop@users.noreply.github.com>
2021-10-02 02:26:36 +0900

    remove dist from git

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-export declare type SyncCallback<T> = (buffer: string[]) => T;
-export declare type AsyncCallback<T> = (buffer: string[]) => Promise<T>;
-export declare type Callback<T> = SyncCallback<T> | AsyncCallback<T>;
-export interface Chunk {
-    toString(...args: any[]): string;
-}
-export declare function capture<T>(writable: NodeJS.WritableStream, callback: (buffer: string[]) => Promise<T>): Promise<T>;
-export declare function capture<T>(writable: NodeJS.WritableStream, callback: (buffer: string[]) => T): T;
